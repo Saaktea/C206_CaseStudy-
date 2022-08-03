ae097fcf8a3da082cfe2e51ed6bcd23ac30361e6
ryanl <ryanl@LAPTOP-5EI43DNM>
2022-08-02 23:56:31 -0700

    commit by ryan

--- C206_CaseStudy/src/student.java
+++ C206_CaseStudy/src/student.java
@@ -1,20 +1,81 @@
 
 public class student {
 
-		private final String STUDENT_NAME;
-		private int studentAge;
-			
-		public Student(String name , int age) {
-			this.STUDENT_NAME = name;
-			this.studentAge = age;
+		private String studentName;
+		private String studentGender;
+		private int studentMobile;
+		private String studentEmail;
+		private int studentDOB;
+		private String studentCountry;
+		private String studentInterest;
+		private String studentFeedback;
+		
+		
+		public student(String studentName , String studentGender , int studentMobile , String studentEmail , int studentDOB , String studentCountry, String studentInterest, String studentFeedback) {
+			this.studentName = studentName;
+			this.studentGender = studentGender;
+			this.studentMobile = studentMobile;
+			this.studentEmail = studentEmail;
+			this.studentDOB = studentDOB;
+			this.studentCountry = studentCountry;
+			this.studentInterest = studentInterest;
+			this.studentFeedback = studentFeedback;
 			
 		}
 		
-		public int getAge{
-			return this.studentAge
-		}
+		
 		
 		public String getStudentName() {
-			return this.STUDENT_NAME;
+			return studentName;
 		}
+		public void setStudentName(String studentName) {
+			this.studentName = studentName;
+		}
+		public String getStudentGender() {
+			return studentGender;
+		}
+		public void setStudentGender(String studentGender) {
+			this.studentGender = studentGender;
+		}
+		public int getStudentMobile() {
+			return studentMobile;
+		}
+		public void setStudentMobile(int studentMobile) {
+			this.studentMobile = studentMobile;
+		}
+		public String getStudentEmail() {
+			return studentEmail;
+		}
+		public void setStudentEmail(String studentEmail) {
+			this.studentEmail = studentEmail;
+		}
+		public int getStudentDOB() {
+			return studentDOB;
+		}
+		public void setStudentDOB(int studentDOB) {
+			this.studentDOB = studentDOB;
+		}
+		public String getStudentCountry() {
+			return studentCountry;
+		}
+		public void setStudentCountry(String studentCountry) {
+			this.studentCountry = studentCountry;
+		}
+		public String getStudentInterest() {
+			return studentInterest;
+		}
+		public void setStudentInterest(String studentInterest) {
+			this.studentInterest = studentInterest;
+		}
+		public String getStudentFeedback() {
+			return studentFeedback;
+		}
+		
+		
+			
+		
+			
+		}
+		
+	
 }
