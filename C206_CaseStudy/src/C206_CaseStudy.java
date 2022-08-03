77856b225720cd5c9170c313880c4ff4fe5cad3f
ryanl <ryanl@LAPTOP-5EI43DNM>
2022-08-02 20:26:35 -0700

    Commited by Ryan

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -106,5 +106,6 @@ public class C206_CaseStudy {
 	}
 	
 	
+	}
 
 }
---
+++ C206_CaseStudy/src/student.java
@@ -0,0 +1,20 @@
+
+public class student {
+
+		private final String STUDENT_NAME;
+		private int studentAge;
+			
+		public Student(String name , int age) {
+			this.STUDENT_NAME = name;
+			this.studentAge = age;
+			
+		}
+		
+		public int getAge{
+			return this.studentAge
+		}
+		
+		public String getStudentName() {
+			return this.STUDENT_NAME;
+		}
+}
