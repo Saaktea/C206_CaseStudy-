a90116527e696efde038d2a08dd0a83f19e8154b
ryanl <ryanl@LAPTOP-5EI43DNM>
2022-08-06 06:30:34 -0700

    committed by ryan

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -22,13 +22,13 @@ public class C206_CaseStudy {
 		subjectGroupList.add(new subjectGroup("Chemistry", "Chem is fun", "Must pass sec1 chem"));
 		
 		//Razi
-		tuitionInfoList .add(new TuitionInfo(1001, "Mathematics", "Math", "Mathematics is the science and study of quality, structure, space, and change.",
-				"1 hour", "Must pass sec1 math", "Mr. Aaron"));
-		tuitionInfoList.add(new TuitionInfo(1002, "Chemistry", "Chemistry",
-				"The branch of science concerned with the substances of which matter is composed, the investigation of their properties and reactions, and the use of such reactions to form new substances.",
-				"45 mins", "Must pass sec1 chem", "Ms. Siti"));
-		
-	
+//		tuitionInfoList .add(new TuitionInfo(1001, "Mathematics", "Math", "Mathematics is the science and study of quality, structure, space, and change.",
+//				"1 hour", "Must pass sec1 math", "Mr. Aaron"));
+//		tuitionInfoList.add(new TuitionInfo(1002, "Chemistry", "Chemistry",
+//				"The branch of science concerned with the substances of which matter is composed, the investigation of their properties and reactions, and the use of such reactions to form new substances.",
+//				"45 mins", "Must pass sec1 chem", "Ms. Siti"));
+//		
+//	
 
 		int option = 0;
 
@@ -38,9 +38,22 @@ public class C206_CaseStudy {
 			option = Helper.readInt("Enter an option > ");
 
 			if (option == 1) {
-				// View all items
-				// ResourceCentre.viewAllCamcorder(camcorderList);
-				// ResourceCentre.viewAllChromebook(chromebookList);
+				C206_CaseStudy.setHeader("Maintan Student Details");
+				studentMenu();
+				Helper.line(80, "-");
+
+				int optionNum = Helper.readInt("Enter option to select > ");
+
+				if (optionNum == 1) {
+					student sm = registerstudent(studentList);
+					C206_CaseStudy.addstudent(studentList, sm);
+				} else if (optionNum == 2) {
+					C206_CaseStudy.viewAllstudent(studentList);
+				} else if (optionNum == 3) {
+					C206_CaseStudy.deletestudent(studentList);
+				} else {
+					System.out.println("Invalid Option Number");
+				}
 
 			} else if (option == 2) {
 				// Add a new item
@@ -175,6 +188,16 @@ public class C206_CaseStudy {
 
 	}
 
+	// ================================= RYAN =================================//
+		public static void studentMenu() {
+			System.out.println("1. Register Student");
+			System.out.println("2. View all Student");
+			System.out.println("3. Delete Student");
+		}
+		
+		
+		// ================================= RYAN =================================//
+		
 	// ================================= SAKTHI =================================//
 	public static void subjectGroupMenu() {
 		System.out.println("1. Register Subject Group");
@@ -212,75 +235,75 @@ public class C206_CaseStudy {
 	
 	// ================================= Razi =================================//
 	
-	public static String retrieveAllTuitionInfo(ArrayList<TuitionInfo> tuitionInfoList) {
-		String output = "";
-
-		for (int i = 0; i < tuitionInfoList.size(); i++) {
-			output += String.format("%-10d %-20s %-10s %-10s %-10s %-10s %-20s\n", tuitionInfoList.get(i).getCode(), tuitionInfoList.get(i).getTitle(),
-					tuitionInfoList.get(i).getGroupName(), tuitionInfoList.get(i).getDescription(), 
-					tuitionInfoList.get(i).getDuration(), tuitionInfoList.get(i).getPreRequisite(), tuitionInfoList.get(i).getTeacher() );
-		}
-		return output;
-	}
+//	public static String retrieveAllTuitionInfo(ArrayList<TuitionInfo> tuitionInfoList) {
+//		String output = "";
+//
+//		for (int i = 0; i < tuitionInfoList.size(); i++) {
+//			output += String.format("%-10d %-20s %-10s %-10s %-10s %-10s %-20s\n", tuitionInfoList.get(i).getCode(), tuitionInfoList.get(i).getTitle(),
+//					tuitionInfoList.get(i).getGroupName(), tuitionInfoList.get(i).getDescription(), 
+//					tuitionInfoList.get(i).getDuration(), tuitionInfoList.get(i).getPreRequisite(), tuitionInfoList.get(i).getTeacher() );
+//		}
+//		return output;
+//	}
+//	
 	
+//	private static void viewAllTuitionInfo(ArrayList<TuitionInfo>tuitionInfoList)
+//	{
+//		C206_CaseStudy.setHeader("TUITION INFORMATION LIST");
+//		Helper.line(80, "-");
+//		
+//		String output = String.format("%-10s %-20s %-10s %-10s %-10s %-10s %-10s\n", "TUITION CODE", "TITLE", 
+//				"TUITION GROUP NAME", "DESCRIPTION", "DURATION", "PRE-REQUISITE", "TEACHER");
+//		output += retrieveAllTuitionInfo(tuitionInfoList);
+//		System.out.println(output);
+//	}
 	
-	private static void viewAllTuitionInfo(ArrayList<TuitionInfo>tuitionInfoList)
-	{
-		C206_CaseStudy.setHeader("TUITION INFORMATION LIST");
-		Helper.line(80, "-");
-		
-		String output = String.format("%-10s %-20s %-10s %-10s %-10s %-10s %-10s\n", "TUITION CODE", "TITLE", 
-				"TUITION GROUP NAME", "DESCRIPTION", "DURATION", "PRE-REQUISITE", "TEACHER");
-		output += retrieveAllTuitionInfo(tuitionInfoList);
-		System.out.println(output);
-	}
-	
-	public static TuitionInfo inputTuitionInfo(ArrayList<TuitionInfo> tuitionInfoList) {
-		int newCode = tuitionInfoList.get(tuitionInfoList.size() - 1).getCode();
+//	public static TuitionInfo inputTuitionInfo(ArrayList<TuitionInfo> tuitionInfoList) {
+//		int newCode = tuitionInfoList.get(tuitionInfoList.size() - 1).getCode();
+//
+//		String title = Helper.readString("Enter Tuition Title > ");
+//		String groupName = Helper.readString("Enter Tuition Group Name > ");
+//		String description = Helper.readString("Enter Tuition Description > ");
+//		String duration = Helper.readString("Enter Tuition Duration > ");
+//		String preRequisite = Helper.readString("Enter Tuition Pre-Requisite > ");
+//		String teacher = Helper.readString("Enter Tuition Teacher > ");
+//
+//		TuitionInfo ti = new TuitionInfo((newCode + 1), title, groupName, description, 
+//				duration, preRequisite, teacher);
 
-		String title = Helper.readString("Enter Tuition Title > ");
-		String groupName = Helper.readString("Enter Tuition Group Name > ");
-		String description = Helper.readString("Enter Tuition Description > ");
-		String duration = Helper.readString("Enter Tuition Duration > ");
-		String preRequisite = Helper.readString("Enter Tuition Pre-Requisite > ");
-		String teacher = Helper.readString("Enter Tuition Teacher > ");
-
-		TuitionInfo ti = new TuitionInfo((newCode + 1), title, groupName, description, 
-				duration, preRequisite, teacher);
-
-		return ti;
-	}
-
-	public static void addTuitionInfo(ArrayList<TuitionInfo> tuitionInfoList, TuitionInfo ti) {
-		tuitionInfoList.add(ti);
-	}
-
-	public static void deleteTuitionInfo(ArrayList<TuitionInfo> tuitionInfoList) {
-		C206_CaseStudy.viewAllTuitionInfo(tuitionInfoList);
-		int codeNum = Helper.readInt("Enter code of tuition > ");
-		Boolean isDeleted = doDeleteTuitionInfo(tuitionInfoList, codeNum);
-
-		if (isDeleted == false) {
-			System.out.println("Invalid entry! Tuition code does not exist");
-		} else {
-			System.out.println("Tuition code " + codeNum + " is deleted!");
-		}
-
-	}
-
-	public static boolean doDeleteTuitionInfo(ArrayList<TuitionInfo> tuitionInfoList, int codeNum) {
-		boolean isDeleted = false;
-
-		for (int i = 0; i < tuitionInfoList.size(); i++) {
-			if (codeNum == (tuitionInfoList.get(i).getCode())) {
-				tuitionInfoList.remove(i);
-
-				isDeleted = true;
-
-			}
-		}
-		return isDeleted;
-	}
+//		return ti;
+//	}
+//
+//	public static void addTuitionInfo(ArrayList<TuitionInfo> tuitionInfoList, TuitionInfo ti) {
+//		tuitionInfoList.add(ti);
+//	}
+//
+//	public static void deleteTuitionInfo(ArrayList<TuitionInfo> tuitionInfoList) {
+//		C206_CaseStudy.viewAllTuitionInfo(tuitionInfoList);
+//		int codeNum = Helper.readInt("Enter code of tuition > ");
+//		Boolean isDeleted = doDeleteTuitionInfo(tuitionInfoList, codeNum);
+//
+//		if (isDeleted == false) {
+//			System.out.println("Invalid entry! Tuition code does not exist");
+//		} else {
+//			System.out.println("Tuition code " + codeNum + " is deleted!");
+//		}
+//
+//	}
+//
+//	public static boolean doDeleteTuitionInfo(ArrayList<TuitionInfo> tuitionInfoList, int codeNum) {
+//		boolean isDeleted = false;
+//
+//		for (int i = 0; i < tuitionInfoList.size(); i++) {
+//			if (codeNum == (tuitionInfoList.get(i).getCode())) {
+//				tuitionInfoList.remove(i);
+//
+//				isDeleted = true;
+//
+//			}
+//		}
+//		return isDeleted;
+//	}
 	
 	// ================================= Razi =================================//
 
@@ -290,7 +313,7 @@ public class C206_CaseStudy {
 
 		for (int i = 0; i < subjectGroupList.size(); i++) {
 			output += String.format("%-10s %-20s %-10s\n", subjectGroupList.get(i).getName(),
-					subjectGroupList.get(i).getDescription(), subjectGroupList.get(i).getPreRequisities());
+					subjectGroupList.get(i).getDescription(), subjectGroupList.get(i).getPrerequisities());
 		}
 		return output;
 	}
@@ -464,5 +487,75 @@ public class C206_CaseStudy {
 		subjectGroupList.add(sg);
 	}
 	// ================================= SAKTHI =================================//
+	
+	// ================================= RYAN ADD =================================//
+		public static student registerstudent(ArrayList<student> studentList) {
+			String name = Helper.readString("Enter your name: ");
+			String gender = Helper.readString("Enter your gender (M/F) : ");
+			int mobile = Helper.readInt("Enter your mobile number:");
+			String email = Helper.readString("Enter your email: ");
+			String DOB = Helper.readString("Enter your Date Of Birth (DD/MM/YYYY) : ");
+			String country = Helper.readString("Enter your country of birth: ");
+			String interest = Helper.readString("Enter your subject of interest : ");
+			String feedback = Helper.readString("Enter your Feedback: ");
+			
 
+			student sm = new student(name, gender, mobile, email , DOB , country , interest , feedback);
+			return sm;
+		}
+		public static void addstudent(ArrayList<student> studentList, student sm) {
+			studentList.add(sm);
+		}
+		
+		
+		
+		// ================================= RYAN ADD =================================//
+		
+		
+		// ================================= RYAN VIEW =================================//
+		public static String retriveAllstudent(ArrayList<student> studentList) {
+			String output = "";
+
+			for (int i = 0; i < studentList.size(); i++) {
+				output += String.format("%-20s %-20s %-20s %-20s %-20s %-20s %-20s %-20s\n", studentList.get(i).getStudentName(),studentList.get(i).getStudentGender(),studentList.get(i).getStudentMobile(),studentList.get(i).getStudentEmail(),
+						studentList.get(i).getStudentDOB(), studentList.get(i).getStudentCountry(), studentList.get(i).getStudentInterest(), studentList.get(i).getStudentFeedback());
+			}
+			return output;
+		}
+
+		public static void viewAllstudent(ArrayList<student> studentList) {
+			C206_CaseStudy.setHeader("STUDENT LIST");
+			String output = String.format("%-20s %-20s %-20s %-20s %-20s %-20s %-20s %-20s\n", "name", "gender", "mobile", "email","DOB","country","interest","feedback");
+			output += retriveAllstudent(studentList);
+			System.out.println(output);
+		}
+		// ================================= RYAN VIEW =================================//
+		// ================================= RYAN DELETE =================================//
+		
+		
+		public static void deletestudent(ArrayList<student> studentList) {
+			C206_CaseStudy.viewAllstudent(studentList);
+			String search = Helper.readString("Enter Student Name: ");
+			Boolean isDeleted = doDeletestudent(studentList, search);
+
+			if (isDeleted == false) {
+				System.out.println("Invalid entry! Student Name does not exist");
+			} else {
+				System.out.println("Subject Group Name " + search + " is deleted!");
+			}
+
+		}
+
+		public static boolean doDeletestudent(ArrayList<student> studentList, String search) {
+			boolean isDeleted = false;
+
+			for (int i = 0; i < studentList.size(); i++) {
+				if (search.equalsIgnoreCase(studentList.get(i).getStudentName()))
+					studentList.remove(i);
+
+				isDeleted = true;
+
+			}
+			return isDeleted;
+		}
 }
